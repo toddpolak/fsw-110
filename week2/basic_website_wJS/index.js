f36d93e8f2080a2c076671eaf4cdcdcc1a31e1e5
toddpolak <67641770+toddpolak@users.noreply.github.com>
2020-09-03 18:52:01 -0700

    Project1 Basic Website Using JS

--- week2/basic_website_wJS/index.js
+++ week2/basic_website_wJS/index.js
@@ -2,7 +2,8 @@ var pageHeader = document.createElement('header');
 var pageNav = document.createElement('nav');
 var pageMain = document.createElement('main');
 var pageFooter = document.createElement('footer');
-var arrFooterLnks = [
+var pageUl = document.createElement('ul')
+var arrPageLinks = [
     ['MDN Web Docs', 'https://developer.mozilla.org/en-US/'],
     ['w3Schools', 'https://www.w3schools.com/'],
     ['VSchool', 'https://coursework.vschool.io/']
@@ -12,29 +13,25 @@ var pageH1 = document.createElement('h1');
 document.title = 'Basic Website Using JS';
 pageH1.textContent = 'Create a Basic Website Using JS';
 
-// Footer Links (loop thru array)
-for (i = 0; i < arrFooterLnks.length; i++){
-    var curItem = arrFooterLnks[i];
-    var footerLink = document.createElement('a');
-    var footerLinkTxt = document.createTextNode(curItem[0]);
-    var footerLinkSpc = document.createTextNode(' | ')
+pageMain.appendChild(pageH1);
 
-    footerLink.appendChild(footerLinkTxt);
-    footerLink.href = curItem[1];
-    pageFooter.appendChild(footerLink);
+// Page Links (loop thru array)
+for (i = 0; i < arrPageLinks.length; i++){
+    var curItem = arrPageLinks[i];
+    var pageLink = document.createElement('a');
+    var pageLinkTxt = document.createTextNode(curItem[0]);
+    var newLi = document.createElement('li')
 
-    if (i + 1 != arrFooterLnks.length) {
-        pageFooter.appendChild(footerLinkSpc);
-    }
-    
-    //document.body.append(pageFooter);
+    pageLink.appendChild(pageLinkTxt);
+    pageLink.href = curItem[1];
+    newLi.appendChild(pageLink)
+    pageUl.appendChild(newLi);
 }
 
-pageMain.appendChild(pageH1);
+pageMain.appendChild(pageUl)
+//document.body.append(pageUl);
 
 document.body.append(pageMain);
 
+pageFooter.innerHTML = '&copy; 2020 Webdev Student';
 document.body.append(pageFooter);
-
-//pageFooter.textContent = '&copy; 2020 Webdev Student';
-//document.body.append(pageFooter);
