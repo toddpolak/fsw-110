71b6568f401ee669a243358596001189cab696df
toddpolak <67641770+toddpolak@users.noreply.github.com>
2020-09-07 10:04:36 -0700

    PnP Turn an Array into a Visual List

---
+++ week3/creating_a_list/index.js
@@ -0,0 +1,31 @@
+document.title = 'Turn an Array into a Visual List';
+
+for(i = 0; i < 10; i++) {
+    var h1Tag = document.createElement('h1');
+
+    h1Tag.textContent = 'Hello World';
+
+    with (h1Tag.style) {
+        color = 'purple';
+        fontFamily = 'cursive';
+        fontStyle = 'italic';
+        fontSize = '1em';
+        textTransform = 'uppercase'; 
+    }
+    document.body.append(h1Tag);
+}
+
+var arrNames = ['steve', 'larry', 'joe', 'shirley', 'steph', 'nate', 'rick', 'emily'];
+var oList = document.createElement('ol');
+
+for (i = 0; i < arrNames.length; i++) {
+    var newLi = document.createElement('li');
+    newLi.textContent = arrNames[i];
+    oList.appendChild(newLi);
+}
+
+with (oList.style) {
+    color = 'red';
+    fontSize = '2em';
+}
+document.body.append(oList);
