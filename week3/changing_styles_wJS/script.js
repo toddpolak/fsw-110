7c90ca5fb7af782f4f6a3dcf23d0b4934e7b4ddd
unknown <todd.polak@bryanuniversity.edu>
2020-09-07 17:01:36 -0700

    PnP Changing Styles and Classes Using JS

---
+++ week3/changing_styles_wJS/script.js
@@ -0,0 +1,16 @@
+var arrH2 = ['Heading1', 'Heading2', 'Heading3', 'Heading4', 'Heading5'];
+
+for (i = 0; i < arrH2.length; i++) {
+    var h2Tag = document.createElement('h2');
+
+    h2Tag.textContent = arrH2[i];
+
+    with (h2Tag.style) {
+        fontSize = '20px';
+        fontWeight = 'lighter';
+        fontFamily = 'sans-serif';
+        color = 'cornflowerblue';
+    }
+
+    document.body.append(h2Tag);
+}
