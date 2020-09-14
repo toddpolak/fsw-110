3df089031d2cefe07180fdaa235105467273a8bb
unknown <todd.polak@bryanuniversity.edu>
2020-09-13 17:08:55 -0700

    PnP Adding Event Listeners

--- week4/dj_js/script.js
+++ week4/dj_js/script.js
@@ -13,3 +13,35 @@ box.addEventListener('mouseover', function() {
 box.addEventListener('mousedown', function() {
     box.style.backgroundColor = 'red';
 })
+
+box.addEventListener('mouseup', function() {
+    box.style.backgroundColor = 'yellow';
+})
+
+box.addEventListener('dblclick', function() {
+    box.style.backgroundColor = 'green';
+})
+
+window.addEventListener('wheel', function() {
+    box.style.backgroundColor = 'orange';
+})
+
+document.addEventListener("keydown", function(event) {
+    switch (event.key.toLowerCase()) {
+        case 'b':
+            box.style.backgroundColor = 'blue';
+            break;
+        case 'r':
+            box.style.backgroundColor = 'red';
+            break;
+        case 'y':
+            box.style.backgroundColor = 'yellow';
+            break;
+        case 'g':
+            box.style.backgroundColor = 'green'
+            break;
+        case 'o':
+            box.style.backgroundColor = 'orange'
+            break;
+    }
+  })
