defcef39b98c55d52ca680292b53798248d54ee0
toddpolak <67641770+toddpolak@users.noreply.github.com>
2020-09-26 18:43:20 -0700

    Travel Form

---
+++ week5/travel_form/script.js
@@ -0,0 +1,27 @@
+const travelForm = document.travel_form;
+
+travelForm.addEventListener("submit", (event) => {
+    event.preventDefault()
+    
+    let firstName = travelForm.first_name.value;
+    let lastName = travelForm.last_name.value;
+    let age = travelForm.age.value;
+    let gender = travelForm.gender.value;
+    let location = travelForm.location.value;
+    let alertMsg;
+    let dietRestrictions = [];
+
+    for (let i = 0; i < travelForm.diet_restrictions.length; i++) {
+        if (travelForm.diet_restrictions[i].checked) {
+            dietRestrictions.push(travelForm.diet_restrictions[i].value);
+        }
+    }
+
+    alertMsg = `First Name: ${firstName}\nLast Name: ${lastName}\nAge: ${age}\nGender: ${gender}\nLocation: ${location}`;
+
+    if (dietRestrictions.length != 0) {
+
+    }
+
+    alert(alertMsg);
+})
