952ec2b6204f85ef3ac323de12227a512ba244cb
toddpolak <67641770+toddpolak@users.noreply.github.com>
2020-09-30 07:30:00 -0700

    Strunctions

--- week6/strunctions/script.js
+++ week6/strunctions/script.js
@@ -16,15 +16,15 @@ function formatPhone(num) {
         + num.slice(3, 6) + '-' + num.slice(6)
 }
 let phoneNum = '9095553598'
-console.log(formatPhone(phoneNum));
+//console.log(formatPhone(phoneNum));
 
-function capitalizeFirstLetter(str) {
-    var strAry = str.toLowerCase().split(' ');
+function properCase(str) {
+    let strAry = str.toLowerCase().split(' ');
     for (var i = 0; i < strAry.length; i++) {
         strAry[i] = strAry[i].charAt(0).toUpperCase() + strAry[i].substr(1);   
     }
    return strAry.join(' ');
 }
 
-let strName = 'todd polak';
-console.log(capitalizeFirstLetter(strName));
+let strName = 'todd polak zappone';
+console.log(properCase(strName));
