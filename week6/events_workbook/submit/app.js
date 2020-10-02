141203ab90e28149ebdbbbf77d46f7fb6de344f9
toddpolak <67641770+toddpolak@users.noreply.github.com>
2020-10-01 22:49:35 -0700

    Events Workbook

--- week6/events_workbook/submit/app.js
+++ week6/events_workbook/submit/app.js
@@ -1,3 +1,13 @@
 // The "submit" event refreshes your browser by default
 // use "e.preventDefault()" in the first line of your callback function to prevent this
 // remember to pass "e" into the callback function
+
+const form = document.getElementById('submit-me');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    let nameInput = document.getElementById('name');
+    nameInput.setAttribute('name', 'user_name');
+    nameInput.setAttribute('id', 'first_name');
+});
