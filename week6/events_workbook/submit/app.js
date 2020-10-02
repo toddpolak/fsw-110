d43a101a088205515c5dc6abe2d38ba4bc373ef2
toddpolak <67641770+toddpolak@users.noreply.github.com>
2020-10-02 11:44:01 -0700

    Events Workbook

--- week6/events_workbook/submit/app.js
+++ week6/events_workbook/submit/app.js
@@ -10,7 +10,4 @@ form.addEventListener('submit', function(e) {
     let nameInput = document.getElementById('name');
     nameInput.setAttribute('name', 'user_name');
     nameInput.setAttribute('id', 'first_name');
-
-    let ageInput = document.getElementById('age');
-    ageInput.setAttribute('type', 'number');
 });
