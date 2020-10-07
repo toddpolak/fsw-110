ee797a9ba5b9cf734b32cd3638fc19befab9c68a
toddpolak <67641770+toddpolak@users.noreply.github.com>
2020-10-07 16:16:27 -0700

    Capstone Project

---
+++ week7/capstone_project/script.js
@@ -0,0 +1,34 @@
+const toDoForm = document.to_do_form;
+
+toDoForm.addEventListener("submit", e => {
+    e.preventDefault()
+
+    let toDoTask = toDoForm.to_do_task.value;
+    let toDoDate = toDoForm.to_do_date.value;
+    let toDoDescription = toDoForm.to_do_description.value;
+    let toDoList = document.getElementById('to_do_list');
+    let newLi = document.createElement('li');
+    let spanTask = document.createElement('span');
+    let spanDate = document.createElement('span')
+    let spanDescription = document.createElement('span');
+    let deleteBtn = document.createElement('button');
+
+    spanTask.textContent = toDoTask;
+    spanDate.textContent = toDoDate;
+    spanDescription.textContent = toDoDescription;
+
+    newLi.appendChild(spanTask);
+    newLi.appendChild(spanDate);
+    newLi.appendChild(spanDescription);
+    newLi.appendChild(deleteBtn);
+    toDoList.appendChild(newLi);
+
+    with (deleteBtn) {
+        innerText = 'Remove';
+        addEventListener('click', e => {
+            e.preventDefault();
+            
+            newLi.remove();
+        });
+    }
+});
