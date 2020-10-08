6683ffe05fe4f0970941ec6da6bbafab0254e642
toddpolak <67641770+toddpolak@users.noreply.github.com>
2020-10-08 12:50:08 -0700

    Capstone Project

--- week7/capstone_project/script.js
+++ week7/capstone_project/script.js
@@ -3,37 +3,42 @@ const toDoForm = document.to_do_form;
 toDoForm.addEventListener("submit", e => {
     e.preventDefault()
 
-    let toDoItem = toDoForm.to_do_item.value;
+    let toDo = toDoForm.to_do.value;
     let toDoDate = toDoForm.to_do_date.value;
     let toDoDescription = toDoForm.to_do_description.value;
     let toDoList = document.getElementById('to_do_list');
     let newLi = document.createElement('li');
-    let spanItem = document.createElement('span');
+    let spanToDo = document.createElement('span');
     let spanDate = document.createElement('span')
     let spanDescription = document.createElement('span');
     let deleteBtn = document.createElement('button');
 
-    spanItem.textContent = toDoItem;
+    spanToDo.style.display = 'block';
+    spanDate.style.display = 'block';
+    spanDescription.style.display = 'block';
+
+    spanToDo.textContent = toDo;
     spanDate.textContent = toDoDate;
     spanDescription.textContent = toDoDescription;
 
     with (newLi) {
-        appendChild(spanItem);
         appendChild(spanDate);
+        appendChild(spanToDo);
         appendChild(spanDescription);
         appendChild(deleteBtn);
     }
     toDoList.appendChild(newLi);
 
+
     with (deleteBtn) {
-        innerText = 'Remove';
+        innerText = 'Delete';
         addEventListener('click', e => {
             newLi.remove();
         });
     }
 
     with (toDoForm) {
-        to_do_item.value = '';
+        to_do.value = '';
         to_do_date.value = '';
         to_do_description.value = '';
     }
