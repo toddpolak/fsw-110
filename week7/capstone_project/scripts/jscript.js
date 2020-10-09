3f74bb8c1073b16121cde3c1135a5fe8d58b2543
toddpolak <67641770+toddpolak@users.noreply.github.com>
2020-10-09 12:15:49 -0700

    Capstone Project

--- week7/capstone_project/scripts/jscript.js
+++ week7/capstone_project/scripts/jscript.js
@@ -4,24 +4,20 @@ toDoForm.addEventListener("submit", e => {
     e.preventDefault()
 
     let toDo = toDoForm.to_do.value;
-    let toDoDate = toDoForm.to_do_date.value;
     let toDoDescription = toDoForm.to_do_description.value;
     let toDoItems = document.getElementById('to_do_items');
     let newDiv = document.createElement('div');
     let spanToDo = document.createElement('span');
-    let spanDate = document.createElement('span');
     let spanDelete = document.createElement('span');
     let spanDescription = document.createElement('span');
     let deleteBtn = document.createElement('button');
 
     newDiv.style.width = '300px';
     spanToDo.style.fontWeight = 'bold';
-    spanToDo.style.padding = '10px';
     spanDelete.style.float = 'right';
     spanDescription.style.display = 'block';
 
     spanToDo.textContent = toDo;
-    spanDate.textContent = toDoDate;
     spanDescription.textContent = toDoDescription;
 
     with (deleteBtn) {
@@ -33,17 +29,14 @@ toDoForm.addEventListener("submit", e => {
     spanDelete.appendChild(deleteBtn);
 
     with (newDiv) {
-        appendChild(spanDate);
         appendChild(spanToDo);
         appendChild(spanDelete);
         appendChild(spanDescription);
-        
     }
     toDoItems.appendChild(newDiv);
 
     with (toDoForm) {
         to_do.value = '';
-        to_do_date.value = '';
         to_do_description.value = '';
     }
 });
